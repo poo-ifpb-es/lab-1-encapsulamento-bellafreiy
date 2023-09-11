b43fa3106b1ce28bfe56fb7721bba49c717aa242
bellafreiy <you@example.com>
2023-09-11 14:50:02 -0300

    Funcionario feita

---
+++ src/main/java/Funcionario.java
@@ -0,0 +1,48 @@
+public class Funcionario {
+    private String nome;
+    private String sobrenome;
+    private float salario;
+
+    public Funcionario(String nome, String sobrenome, float salario) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public float getSalarioAnual() {
+        return salario * 12;
+    }
+
+    public void concederAumento(float valor) {
+        if (valor >= 0 && valor <= 100) {
+            float aumento = (valor / 100) * salario;
+            salario += aumento;
+        } else {
+            System.out.println("Valor de aumento inválido. Deve estar entre 0 e 100.");
+        }
+    }
+}
